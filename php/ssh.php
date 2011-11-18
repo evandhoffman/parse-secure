693da1c94488f295ed118e238a1741a2f82dcd03
Evan D. Hoffman <evandhoffman@gmail.com>
2011-11-18 13:28:16 -0500

    Took out GeoIP stuff, which is now in the DB.

--- php/ssh.php
+++ php/ssh.php
@@ -2,10 +2,9 @@ SSH hacking attempts
 <?php
 require_once "Net/GeoIP.php";
 
-$geoip = Net_GeoIP::getInstance("/var/www/html/GeoLiteCity.dat", Net_GeoIP::SHARED_MEMORY);
 
 	$dbc = pg_connect("dbname=sshlog host=127.0.0.1 user=sshlog password=sshlog");
-	$res = pg_query("select * from ssh_hack_attempts order by datetime desc limit 500");
+	$res = pg_query("select * from ssh_hack_attempts order by datetime desc limit 30");
 
 	if (!$res) {
 		echo "DB Error.  Sadface.\n";
@@ -14,9 +13,8 @@ $geoip = Net_GeoIP::getInstance("/var/www/html/GeoLiteCity.dat", Net_GeoIP::SHAR
 
 	print "<table border='1'><tr><td>Date</td><td>IP</td><td>User</td><td>Where?</td></tr>";
 	while ($row = pg_fetch_assoc($res)) {
-		$location = $geoip->lookupLocation($row['remote_addr']);
 		//print_r($location);
-		print "<tr><td>$row[datetime]</td><td>$row[remote_addr]</td><td>$row[username]</td><td>$location->countryName, $location->city</td></tr>\n";
+		print "<tr><td>$row[datetime]</td><td>$row[remote_addr]</td><td>$row[username]</td><td>$row[country_name], $row[city]</td></tr>\n";
 	}
 	print "</table>\n";
 ?>
